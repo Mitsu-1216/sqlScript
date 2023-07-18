de6765a4416fd9ac7a6ed8ad40972d50a7d2dca1
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-07-19 07:58:15 +0900

    SELECT

--- src/kadai/application.sql
+++ src/kadai/application.sql
@@ -18,3 +18,14 @@ INSERT INTO 審判
 VALUES ('佐藤健太', 'X')
 , ('鈴木翔太', 'Y')
 , ('高橋卓也', 'Z');
+
+SELECT
+    A.チーム名 AS 出場チーム1
+    , B.チーム名 AS 出場チーム2
+    , C.氏名 AS 審判氏名 
+FROM
+    サッカーチーム AS A
+    , サッカーチーム AS B
+    , 審判 AS C 
+WHERE
+    A.チーム名 < b.チーム名 
