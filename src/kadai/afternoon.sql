d7a12bb3690a362621ff07c0e20faa67a639b291
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-08-18 08:27:51 +0900

    ADD CONSTRAINT

--- src/kadai/afternoon.sql
+++ src/kadai/afternoon.sql
@@ -1,4 +1,5 @@
 -- GRANT オブジェクトの権限 ON オブジェクト名 TO { ユーザ名 | ロール名 | PUBLIC }[ WITH GRANT OPTION ]
 GRANT SELECT(氏名,所属チーム名) ON 審判 TO user;
 
-ALTER TABLE
+-- 制約付与
+ALTER TABLE テーブル名 ADD CONSTRAINT 制約名 制約の内容
