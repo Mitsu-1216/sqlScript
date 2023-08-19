87faabca87c378442554e7083255f65d5ca75d19
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-08-19 14:23:14 +0900

    1aut

--- src/kadai/afternoon.sql
+++ src/kadai/afternoon.sql
@@ -5,3 +5,7 @@ GRANT SELECT(氏名,所属チーム名) ON 審判 TO user;
 ALTER TABLE テーブル名 ADD CONSTRAINT 制約名 制約の内容;
 
 WITH句はWITH 相関名 AS (SELECT文);
+
+INSERT INTO 月次レポート(従業員番号,レポート年月)
+SELECT 従業員番号,：レポート年月
+FROM 従業員
