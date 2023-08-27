4265eca4b28daef2be8607542f6f867113cbce04
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-08-27 11:02:07 +0900

    WTH

--- src/kadai/afternoon.sql
+++ src/kadai/afternoon.sql
@@ -4,6 +4,8 @@ GRANT SELECT(氏名,所属チーム名) ON 審判 TO user;
 -- 制約付与
 ALTER TABLE テーブル名 ADD CONSTRAINT 制約名 制約の内容;
 
+/*WITH 副問合せ名 AS (クエリ文)：副問合せ名」を記述することにより、
+ASの直後のクエリ文を何度も記述することなく使い回すことが可能*/
 WITH句はWITH 相関名 AS (SELECT文);
 
 INSERT INTO 月次レポート(従業員番号,レポート年月)
