1c13c7f5519c0475304692e7adeb764bb8f405fd
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-09-10 09:31:30 +0900

    CRROSS JOIN

--- src/kadai/application_2.sql
+++ src/kadai/application_2.sql
@@ -9,7 +9,7 @@ VALUES ('D01', 1000, 4000)
 , ('D02', 2000, 5000)
 , ('D03', 3000, 8000);
 
-
+--ア
 SELECT
     部門コード
     , '第1期' AS 期
@@ -24,6 +24,64 @@ INTERSECT (
     FROM
         部門別売上
 ) 
+ORDER BY
+    部門コード
+    , 期
+
+--イ
+SELECT
+    部門コード
+    , '第1期' AS 期
+    , 第1期売上 AS 売上 
+FROM
+    部門別売上 
+UNION ( 
+
+    SELECT
+        部門コード
+        , '第2期' AS 期
+        , 第2期売上 AS 売上 
+    FROM
+        部門別売上
+) 
+ORDER BY
+    部門コード
+    , 期
+
+--ウ
+SELECT
+    A.部門コード
+    , '第1期' AS 期
+    , 第1期売上 AS 売上 
+FROM
+    部門別売上 A
+CROSS JOIN ( 
+    SELECT
+        B.部門コード
+        , '第2期' AS 期
+        , B.第2期売上 AS 売上 
+    FROM
+        部門別売上 B
+) T
+ORDER BY
+    部門コード
+    , 期
+
+--エ
+SELECT
+    A.部門コード
+    , '第1期' AS 期
+    , 第1期売上 AS 売上 
+FROM
+    部門別売上 A
+INNER JOIN ( 
+    SELECT
+        B.部門コード
+        , '第2期' AS 期
+        , B.第2期売上 AS 売上 
+    FROM
+        部門別売上 B
+) T
 ORDER BY
     部門コード
     , 期
