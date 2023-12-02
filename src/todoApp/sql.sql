6653c6fab28ab54c109ca14b517856e4680bdd3e
Mitsu-1216 <adieuer._.03.26@icloud.com>
2023-12-02 14:12:39 +0900

    CREATE DATABASE,CREATE TABLE

---
+++ src/todoApp/sql.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE todo_app;
+
+USE todo_app;
+
+CREATE TABLE users( 
+    userid INTEGER
+    , name VARCHAR(100)
+    , password VARCHAR(100)
+)
+
+-- INSERT INTO 家計簿 
+-- VALUES ('2023/7/04', NULL, 'コーヒーを飲んだ', 100, 100)
